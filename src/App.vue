4e8734a92d238c4fd75fe7254d6145f92243cbfa
devwithu <devwithu@naver.com>
2021-06-16 16:19:08 +0900

    chapter7 server binding data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/about">About</router-link> -->
+    <router-link to="/databindinglist2">Data Binding List2</router-link>
   </div>
   <router-view/>
 </template>
---
+++ src/views/DataBinding.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello, {{title}}</h1>
+</template>
+<script>
+export default {
+    name:'',
+    components: {},
+    data() {
+        return {
+            title: 'World'
+        };
+    },
+    beforeCreate() {},
+    create() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeUnmount() {},
+    unmounted() {},
+    methods: {}
+}
+</script>
